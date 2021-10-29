fb3b712352ca863a66b317604da7ad9dfa5b5f18
VcatYuki <neusbosk@gmail.com>
2021-10-29 12:03:02 +0200

    Mobile

---
+++ Assets/js/sound.js
@@ -0,0 +1,8 @@
+/* const music = new Audio('adf.wav');
+music.play();
+music.loop =true;
+music.playbackRate = 2;
+music.pause();qqazszdgfbgtyj */
+/* 
+var audio = new Audio('audio.mp3');
+  audio.play(); */
