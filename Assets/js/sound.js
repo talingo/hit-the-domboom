0ee3b0961f627e421c6c633b058d87d322e221c4
VcatYuki <neusbosk@gmail.com>
2021-11-04 10:04:59 +0100

    javascript

--- Assets/js/sound.js
+++ Assets/js/sound.js
@@ -7,17 +7,22 @@ music.pause(); */
 var audio = new Audio('audio.mp3');
   audio.play(); */
 
-const bombo = document.querySelector('#hitd');
-bombo.addEventListener('click',function (ev) {    
-  alert('click')
-  if (ev.target.tagName === 'img'){
-    alert(ev)
-    var audio = new Audio('./Assets/sound/bombo (1).wav');
-    audio.play();
-  }
- 
+const bombo = document.querySelector('#bombo');
+bombo.addEventListener('click',function () {    
+  alert('click')    
+  var audio = new Audio('./Assets/sound/bombo (1).wav');
+  audio.play();  
 },
 false
 );
 
-let hitted = document.querySelectorAll ('.hit')
+/* hit mobile */
+
+const hitm = document.querySelector('#hitm');
+hitm.addEventListener('click',function () {    
+  alert('click')    
+  var audio = new Audio('./Assets/sound/bombo (1).wav');
+  audio.play();  
+},
+false
+);
