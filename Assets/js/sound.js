38e6b5c1d354cc91293e12d14bc6c51f3a96002d
VcatYuki <neusbosk@gmail.com>
2021-11-04 10:25:28 +0100

    audio all

--- Assets/js/sound.js
+++ Assets/js/sound.js
@@ -7,19 +7,86 @@ music.pause(); */
 var audio = new Audio('audio.mp3');
   audio.play(); */
 
-const bombo = document.querySelector('#bombo');
-bombo.addEventListener('click',function () {    
+  /* Bombo */
+const bombo = document.querySelector('#hitbombo');
+bombo.addEventListener('click',function() {    
   alert('click')    
   var audio = new Audio('./Assets/sound/bombo (1).wav');
   audio.play();  
 },
 false
 );
+/* Crash */
+const crash = document.querySelector('#hitcrash');
+crash.addEventListener('click',function() {    
+  alert('click')    
+  var audio = new Audio('./Assets/sound/crash.wav');
+  audio.play();  
+},
+false
+);
+/* hit-hat */
+const hithat = document.querySelector('#hithat');
+hithat.addEventListener('click',function() {    
+  alert('click')    
+  var audio = new Audio('./Assets/sound/hit-hat.wav');
+  audio.play();  
+},
+false
+);
+/* redoble */
+const redo = document.querySelector('#hitredo');
+redo.addEventListener('click',function() {    
+  alert('click')    
+  var audio = new Audio('./Assets/sound/hit-hat.wav');
+  audio.play();  
+},
+false
+);
+/* tom1 */
+const tom1 = document.querySelector('#hittom1');
+tom1.addEventListener('click',function() {    
+  alert('click')    
+  var audio = new Audio('./Assets/sound/tom1.wav');
+  audio.play();  
+},
+false
+);
+
+/* tom2 */
+const tom2 = document.querySelector('#hittom2');
+tom2.addEventListener('click',function() {    
+  alert('click')    
+  var audio = new Audio('./Assets/sound/tom2.wav');
+  audio.play();  
+},
+false
+);
+
+/* tom3 */
+const tom3 = document.querySelector('#hittom3');
+tom3.addEventListener('click',function() {    
+  alert('click')    
+  var audio = new Audio('./Assets/sound/tom3.wav');
+  audio.play();  
+},
+false
+);
+/* tom4 */
+const tom4 = document.querySelector('#hittom4');
+tom4.addEventListener('click',function() {    
+  alert('click')    
+  var audio = new Audio('./Assets/sound/tom4.wav');
+  audio.play();  
+},
+false
+);
+
 
 /* hit mobile */
 
 const hitm = document.querySelector('#hitm');
-hitm.addEventListener('click',function () {    
+hitm.addEventListener('click',function() {    
   alert('click')    
   var audio = new Audio('./Assets/sound/bombo (1).wav');
   audio.play();  
