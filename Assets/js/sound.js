34e072a589e8365a680ea473d385f1f64911cd9b
VcatYuki <neusbosk@gmail.com>
2021-11-04 09:43:42 +0100

    sounds

--- Assets/js/sound.js
+++ Assets/js/sound.js
@@ -2,7 +2,22 @@
 music.play();
 music.loop =true;
 music.playbackRate = 2;
-music.pause();qqazszdgfbgtyj */
+music.pause(); */
 /* 
 var audio = new Audio('audio.mp3');
   audio.play(); */
+
+const bombo = document.querySelector('#hitd');
+bombo.addEventListener('click',function (ev) {    
+  alert('click')
+  if (ev.target.tagName === 'img'){
+    alert(ev)
+    var audio = new Audio('./Assets/sound/bombo (1).wav');
+    audio.play();
+  }
+ 
+},
+false
+);
+
+let hitted = document.querySelectorAll ('.hit')
